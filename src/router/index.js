16ab86e282659db7f8729610b5832128413c543f
12Non-no <nsuzuki@dl-techx-xedge.net>
2025-09-11 18:06:51 +0900

    index.js変更

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import MyPageView from '../views/MyPageView.vue'
 import SignupView from '../views/SignupView.vue'
@@ -8,13 +8,13 @@ import SignupView from '../views/SignupView.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView
+  // },
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/login',
     name: 'login',
     component: LoginView
   },
@@ -27,14 +27,6 @@ const routes = [
     path: '/signup',
     name: 'signup',
     component: SignupView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
